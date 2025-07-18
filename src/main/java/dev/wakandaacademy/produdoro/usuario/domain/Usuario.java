c866350240910a9465e61e359a31411c969638e1
Felipe Moraski <moraski060@gmail.com>
2025-07-18 17:21:51 -0300

    feat: implementa camada Service e metodo muda status para foco em dominio de Usuario

--- src/main/java/dev/wakandaacademy/produdoro/usuario/domain/Usuario.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/domain/Usuario.java
@@ -1,21 +1,16 @@
 package dev.wakandaacademy.produdoro.usuario.domain;
 
-import java.util.UUID;
-
-import javax.validation.constraints.Email;
-
+import dev.wakandaacademy.produdoro.handler.APIException;
+import dev.wakandaacademy.produdoro.pomodoro.domain.ConfiguracaoPadrao;
+import dev.wakandaacademy.produdoro.usuario.application.api.UsuarioNovoRequest;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.http.HttpStatus;
 
-import dev.wakandaacademy.produdoro.pomodoro.domain.ConfiguracaoPadrao;
-import dev.wakandaacademy.produdoro.usuario.application.api.UsuarioNovoRequest;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import javax.validation.constraints.Email;
+import java.util.UUID;
 
 @Builder
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -24,21 +19,39 @@ import lombok.ToString;
 @ToString
 @Document(collection = "Usuario")
 public class Usuario {
-	@Id
-	private UUID idUsuario;
-	@Email
-	@Indexed(unique = true)
-	private String email;
-	private ConfiguracaoUsuario configuracao;
-	@Builder.Default
-	private StatusUsuario status = StatusUsuario.FOCO;
-	@Builder.Default
-	private Integer quantidadePomodorosPausaCurta = 0;
-	
-	public Usuario(UsuarioNovoRequest usuarioNovo, ConfiguracaoPadrao configuracaoPadrao) {
-		this.idUsuario = UUID.randomUUID();
-		this.email = usuarioNovo.getEmail();
-		this.status = StatusUsuario.FOCO;
-		this.configuracao = new ConfiguracaoUsuario(configuracaoPadrao);
-	}
+    @Id
+    private UUID idUsuario;
+    @Email
+    @Indexed(unique = true)
+    private String email;
+    private ConfiguracaoUsuario configuracao;
+    @Builder.Default
+    private StatusUsuario status = StatusUsuario.FOCO;
+    @Builder.Default
+    private Integer quantidadePomodorosPausaCurta = 0;
+
+    public Usuario(UsuarioNovoRequest usuarioNovo, ConfiguracaoPadrao configuracaoPadrao) {
+        this.idUsuario = UUID.randomUUID();
+        this.email = usuarioNovo.getEmail();
+        this.status = StatusUsuario.FOCO;
+        this.configuracao = new ConfiguracaoUsuario(configuracaoPadrao);
+    }
+
+    public void mudaStatusParaFoco(UUID idUsuario) {
+        validaUsuario(idUsuario);
+        verificaStatus(StatusUsuario.FOCO);
+        this.status = StatusUsuario.FOCO;
+    }
+
+    private void verificaStatus(StatusUsuario statusUsuario) {
+        if(this.status.equals(statusUsuario)){
+            throw APIException.build(HttpStatus.CONFLICT, "Usuário já está em FOCO");
+        }
+    }
+
+    private void validaUsuario(UUID idUsuario) {
+        if (!this.idUsuario.equals(idUsuario)) {
+            throw APIException.build(HttpStatus.UNAUTHORIZED, "credencial de autenticação não é valida");
+        }
+    }
 }

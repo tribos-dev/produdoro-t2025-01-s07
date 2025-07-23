667dbb1c946e9862d4d2df593eaa77ee57ce2519
Zelto Lira <zelto16@hmail.com>
2025-07-22 21:55:48 -0300

    feat: implementa camada ApplicationService

--- src/main/java/dev/wakandaacademy/produdoro/usuario/domain/Usuario.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/domain/Usuario.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import javax.validation.constraints.Email;
 
+import dev.wakandaacademy.produdoro.handler.APIException;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -16,6 +17,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.http.HttpStatus;
 
 @Builder
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -40,5 +42,23 @@ public class Usuario {
 		this.email = usuarioNovo.getEmail();
 		this.status = StatusUsuario.FOCO;
 		this.configuracao = new ConfiguracaoUsuario(configuracaoPadrao);
+	}
+
+    public void mudaStatusParaPausaLonga(UUID idUsuario) {
+		validarUsuario(idUsuario);
+		validarStatusPausaLonga();
+		this.status = StatusUsuario.PAUSA_LONGA;
+    }
+
+	private void validarStatusPausaLonga() {
+		if (this.status.equals(StatusUsuario.PAUSA_LONGA)){
+			throw APIException.build(HttpStatus.CONFLICT, "Usuário já está em pausa longa");
+		}
+	}
+
+	private void validarUsuario(UUID idUsuario) {
+		if (!this.idUsuario.equals(idUsuario)){
+			throw APIException.build(HttpStatus.UNAUTHORIZED, "Usuário(a) não autorizado(a) para a requisição solicitada");
+		}
 	}
 }

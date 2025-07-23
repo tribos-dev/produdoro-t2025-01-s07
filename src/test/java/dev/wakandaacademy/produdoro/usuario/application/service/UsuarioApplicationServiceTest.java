4802929c1cd162a0bcaafe419a5c8d9b59f1baa7
Zelto Lira <zelto16@hmail.com>
2025-07-22 23:06:36 -0300

    test: Implementa teste deveMudarStatusParaPausaLonga. Teste Ok

--- src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
@@ -1,7 +1,50 @@
 package dev.wakandaacademy.produdoro.usuario.application.service;
 
+import dev.wakandaacademy.produdoro.DataHelper;
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+import dev.wakandaacademy.produdoro.usuario.domain.StatusUsuario;
+import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class UsuarioApplicationServiceTest {
+    @InjectMocks
+    UsuarioApplicationService usuarioApplicationService;
+
+    @Mock
+    UsuarioRepository usuarioRepository;
+
+    private Usuario usuarioMock;
+    private  final String usuarioEmail = "usuario@teste.com";
+    private  final UUID idUsuario = UUID.randomUUID();
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.openMocks(this);
+        usuarioMock = mock(Usuario.class);
+        when(usuarioRepository.buscaUsuarioPorEmail(usuarioEmail)).thenReturn(usuarioMock);
+    }
+
+    @Test
+    void deveMudarStatusParaPausaLonga(){
+        Usuario usuario = DataHelper.createUsuarioFoco();
+        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+        when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
+        usuarioApplicationService.mudaStatusParaPausaLonga(usuario.getEmail(), usuario.getIdUsuario());
+
+        verify(usuarioRepository, times(1)).salva(usuario);
+        assertEquals(StatusUsuario.PAUSA_LONGA, usuario.getStatus());
+    }
+
+
 
 }

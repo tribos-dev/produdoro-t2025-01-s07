07ca3e9b17cbf42530c97879ac0c44e31198f6b3
Zelto Lira <zelto16@hmail.com>
2025-07-25 07:53:45 -0300

    test: implementa testes para deveIncrementarPomodoro e naoDeveIncrementarPomodoro_tarefaNaoEncontrada

--- src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
@@ -1,15 +1,19 @@
 package dev.wakandaacademy.produdoro.tarefa.application.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+import dev.wakandaacademy.produdoro.DataHelper;
+import dev.wakandaacademy.produdoro.config.security.service.TokenService;
+import dev.wakandaacademy.produdoro.handler.APIException;
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -32,6 +36,12 @@ class TarefaApplicationServiceTest {
     @Mock
     TarefaRepository tarefaRepository;
 
+    @Mock
+    UsuarioRepository usuarioRepository;
+
+    @Mock
+    TokenService tokenService;
+
     @Test
     void deveRetornarIdTarefaNovaCriada() {
         TarefaRequest request = getTarefaRequest();
@@ -49,5 +59,51 @@ class TarefaApplicationServiceTest {
     public TarefaRequest getTarefaRequest() {
         TarefaRequest request = new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
         return request;
+    }
+
+    @Test
+    void deveIncrementarUmPomodoroATarefa(){
+        //cenario
+        Usuario usuario = DataHelper.createUsuarioFoco();
+        Tarefa tarefa = DataHelper.createTarefa();
+        when(usuarioRepository.buscaUsuarioPorEmail(usuario.getEmail())).thenReturn(usuario);
+        when(tarefaRepository.buscaTarefaPorId(tarefa.getIdTarefa())).thenReturn(Optional.of(tarefa));
+        when(usuarioRepository.salva(usuario)).thenReturn(usuario);
+        when(tarefaRepository.salva(tarefa)).thenReturn(tarefa);
+
+        //acao
+        tarefaApplicationService.incrementaPomodoro(usuario.getEmail(), tarefa.getIdTarefa());
+
+        //verificacao
+        assertEquals(2, tarefa.getContagemPomodoro(), "Deveria incrementar 1 pomodoro");
+
+        verify(usuarioRepository, times(2)).buscaUsuarioPorEmail(usuario.getEmail());
+        verify(tarefaRepository).buscaTarefaPorId(tarefa.getIdTarefa());
+        verify(usuarioRepository).salva(usuario);
+        verify(tarefaRepository).salva(tarefa);
+    }
+
+    @Test
+    void naoDeveIncrementarPomodoro_tarefaNaoEncontrada(){
+        //cenario
+        Usuario usuario = DataHelper.createUsuarioFoco();
+        UUID idTarefaInvalido = UUID.randomUUID();
+        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+        when(tarefaRepository.buscaTarefaPorId(idTarefaInvalido)).thenReturn(Optional.empty());
+
+        //acao
+        APIException exception = assertThrows(APIException.class,
+                () -> tarefaApplicationService.incrementaPomodoro(usuario.getEmail(), idTarefaInvalido));
+
+        //verificacao
+        assertEquals("Tarefa não encontrada!", exception.getBodyException().getMessage());
+        verify(usuarioRepository, times(1)).buscaUsuarioPorEmail(usuario.getEmail());
+        verify(tarefaRepository, times(1)).buscaTarefaPorId(idTarefaInvalido);
+    }
+
+    @Test
+    void naoDeveIncrementarPomodoro_usuarioNaoAutorizado() {
+        //cenario
+
     }
 }
